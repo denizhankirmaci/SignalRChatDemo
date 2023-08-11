1ad0d6bea3abe5be84073c965b58bb50aa4ba41f
Denizhan KIRMACI <denizhan.kirmaci@mytechnic.aero>
2023-08-11 13:33:13 +0300

    js değişiklikleri

--- SignalRChatDemo/Hubs/ChatHub.cs
+++ SignalRChatDemo/Hubs/ChatHub.cs
@@ -4,9 +4,19 @@ namespace SignalRChatDemo.Hubs
 {
     public class ChatHub : Hub
     {
-        public async Task SendMessage(string user, string message)
+        public async Task SendMessageAll(string senderId, string message)
         {
-            await Clients.All.SendAsync("ReceiveMessage", user, message);
+            await Clients.All.SendAsync("ReceiveMessage", senderId, message);
+        }
+
+        public async Task SendMessageGroup(string groupId, string senderId, string message)
+        {
+            await Clients.Group(groupId).SendAsync("ReceiveMessage", senderId, message);
+        }
+
+        public Task JoinGroup(string groupName)
+        {
+            return Groups.AddToGroupAsync(Context.ConnectionId, groupName);
         }
     }
 }
